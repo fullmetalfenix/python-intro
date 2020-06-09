837e04732bddd5243ad7ef36dde6fb5e64182049
J Pihlk <jpihlk@yahoo.com>
2020-06-09 11:58:31 -0400

    added error handling

--- master_ticket.py
+++ master_ticket.py
@@ -3,31 +3,25 @@ TICKET_PRICE = 10
 tickets_remaining = 100
 
 while tickets_remaining >= 1:
-    # output tickets remaining
     print("There are {} Tickets remaining  ".format(tickets_remaining))
-
-    #gather usere name and assign to var
-
     name = input("What is your name?  ")
-
-    # Prompt user by name about ticket amount
     ticket_quantity = input("OK {}, How many tickets would you like?  ".format(name))
-    ticket_quantity = int(ticket_quantity)
-    # Calculate price (tickets * price) and assign to variable
-    total_cost = ticket_quantity * TICKET_PRICE
-
-    # Output the price to the screen
-
-    print("Your total cost is {}.  ".format(total_cost))
-
-
-    procede_with_purchase = input("Do you want to procede with your purchase? Y/N  ")
-
-    if procede_with_purchase.upper() == "Y":
-        tickets_remaining -= ticket_quantity
-        # process CC
-        print("Sold! ")
-    else :
-        print("Thank you for your interest {}  ".format(name))
-# Close while block
+    # Handle ValueError here
+    try:
+        ticket_quantity = int(ticket_quantity)
+        # raise WalueError if sake is form more tickets then available
+        if ticket_quantity > tickets_remaining:
+            raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
+    except ValueError as err:
+        print("Oh no there has been an error {}, please try again!".format(err))
+    else:
+        total_cost = ticket_quantity * TICKET_PRICE
+        print("Your total cost is {}.  ".format(total_cost))
+        procede_with_purchase = input("Do you want to procede with your purchase? Y/N  ")
+        if procede_with_purchase.upper() == "Y":
+            tickets_remaining -= ticket_quantity
+            # process CC
+            print("Sold! ")
+        else :
+            print("Thank you for your interest {}  ".format(name))
 print("Sorry, tickets are all sold out!  ")
